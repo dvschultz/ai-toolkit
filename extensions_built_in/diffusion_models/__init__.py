00ac47eabe6a0ce79582cc39f8a2da8b6280c1a0
Derrick Schultz <dvsmethid@gmail.com>
2026-08-12 10:01:49 +0000

    chore: sync upstream ai-toolkit main

--- extensions_built_in/diffusion_models/__init__.py
+++ extensions_built_in/diffusion_models/__init__.py
@@ -18,6 +18,8 @@ from .ideogram4 import Ideogram4Model
 from .prx_pixel_t2i import PRXPixelT2IModel
 from .krea2 import Krea2Model
 from .boogu_image import BooguImageModel, BooguImageEditModel
+from .mageflow import MageFlowModel, MageFlowEditModel
+from .minimax_h3 import MinimaxH3Model
 
 AI_TOOLKIT_MODELS = [
     # put a list of models here
@@ -51,4 +53,7 @@ AI_TOOLKIT_MODELS = [
     Krea2Model,
     BooguImageModel,
     BooguImageEditModel,
+    MageFlowModel,
+    MageFlowEditModel,
+    MinimaxH3Model,
 ]
